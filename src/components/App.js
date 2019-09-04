85d5dd9de2cdf552e37ed6d01cc8549452ea5bbb
Jacob <kang2@o2.pl>
2019-09-04 12:57:56 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from '../history';
+
+import './App.scss'
+import ShowProfile from './profile/ShowProfile';
+
+
+// To do
+// Pick profile from random id 
+//<Route path="/profile/:id" exact component={ShowProfile} />
+
+const App = () => {
+    return (
+        <div className="ui container">
+			<Router history={history}>
+				<Switch>
+					<Route path="/" exact component={ShowProfile} />
+				</Switch>
+			</Router>
+		</div>
+    )
+};
+
+export default App;
---
+++ src/components/profile/ShowProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ShowProfile.scss';
+
+class ShowProfile extends React.Component {
+    render() {
+        return (
+            <div className="container__wrapper">App</div>
+        )
+    }
+}
+
+export default ShowProfile;
